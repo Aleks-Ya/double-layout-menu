caec7c07b236815153c0e4abffbb87d26fd508ed
Aleksey Yablokov <ya_al@bk.ru>
2014-09-01 12:26:34 +0400

    test Вынес метод createEntryFile в Helper

--- test/ru/yaal/doublelayoutmenu/EntryPersistHelperTest.java
+++ test/ru/yaal/doublelayoutmenu/EntryPersistHelperTest.java
@@ -1,42 +1,24 @@
 package ru.yaal.doublelayoutmenu;
 
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Properties;
 
 import static org.testng.Assert.assertEquals;
 import static ru.yaal.doublelayoutmenu.Helper.content;
+import static ru.yaal.doublelayoutmenu.Helper.createEntryFile;
 
 public class EntryPersistHelperTest {
 
-    private File entryFile;
-    private Path entryPath;
     private String name = "Skype";
     private String comment = "Skype Internet Telephony";
 
-    @BeforeMethod
-    public void setUp() throws Exception {
-        entryFile(content(name, comment));
-    }
-
-    private void entryFile(String content) throws IOException {
-        entryFile = File.createTempFile("DoubleLayoutMenu_", ".tmp");
-        entryFile.deleteOnExit();
-        entryPath = entryFile.toPath();
-
-        Files.write(entryPath, Arrays.asList(content));
-    }
-
     @Test
     public void read() throws Exception {
-        Entry entry = EntryPersistHelper.read(entryPath.toFile());
+        File entryFile = createEntryFile(content(name, comment));
+        Entry entry = EntryPersistHelper.read(entryFile);
         assertEquals(entry.getName(), name);
         assertEquals(entry.getComment(), comment);
         assertEquals(entry.getFile(), entryFile);
@@ -44,8 +26,10 @@ public class EntryPersistHelperTest {
 
     @Test
     public void save() throws Exception {
+        File entryFile = createEntryFile(content(name, comment));
+
         //expected
-        entryFile(content("Telegram", "Telegram Messenger"));
+        createEntryFile(content("Telegram", "Telegram Messenger"));
 
         Properties expProperties = new Properties();
         expProperties.load(new FileInputStream(entryFile));
@@ -53,7 +37,7 @@ public class EntryPersistHelperTest {
         expProperties.setProperty("Comment", comment);
 
         //actual
-        Entry entry = EntryPersistHelper.read(entryPath.toFile());
+        Entry entry = EntryPersistHelper.read(entryFile);
         entry.setName(name);
         entry.setComment(comment);
 
--- test/ru/yaal/doublelayoutmenu/Helper.java
+++ test/ru/yaal/doublelayoutmenu/Helper.java
@@ -1,5 +1,10 @@
 package ru.yaal.doublelayoutmenu;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 class Helper {
      static String content(String name, String comment) {
         return "[Desktop Entry]\n" +
@@ -13,5 +18,13 @@ class Helper {
                 "Categories=Network;Application;\n" +
                 "MimeType=x-scheme-handler/skype;\n" +
                 "X-KDE-Protocols=skype";
+    }
+
+    static File createEntryFile(String content) throws IOException {
+        File entryFile = File.createTempFile("DoubleLayoutMenu_", ".tmp");
+        entryFile.deleteOnExit();
+
+        Files.write(entryFile.toPath(), Arrays.asList(content));
+        return entryFile;
     }
 }
