cff8b9b1ed74167d71f3cc4e11bbfbe8002c0426
Aleksey Yablokov <ya_al@bk.ru>
2015-04-13 09:44:32 +0300

    refactor package access for DesktopFilenameFilter

--- src/ru/yaal/doublelayoutmenu/DesktopFilenameFilter.java
+++ src/ru/yaal/doublelayoutmenu/DesktopFilenameFilter.java
@@ -3,7 +3,7 @@ package ru.yaal.doublelayoutmenu;
 import java.io.File;
 import java.io.FilenameFilter;
 
-public class DesktopFilenameFilter implements FilenameFilter {
+class DesktopFilenameFilter implements FilenameFilter {
     @Override
     public boolean accept(File dir, String name) {
         return name.endsWith(".desktop");
